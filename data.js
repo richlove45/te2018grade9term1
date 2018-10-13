0a05cf493840850413f893f84f145cb98d8b61c9
richlove45 <richlovenkansah@gmail.com>
2018-10-13 15:41:58 -0400

    about myself

--- data.js
+++ data.js
@@ -2,4 +2,32 @@
 let message = "poorhate";
 console.log(message);
 
+let name = "richlove"
+console.log(name);
+
+let nickname = "richierich"
+console.log(nickname);
+
+let catchphrase = true;
+console.log(catchphrase);
+
+let likespeople = false;
+console.log(likespeople);
+
+let Age = 14
+console.log(14);
+
+let yearsinamerica=5
+console.log(yearsinamerica);
+
+let favoritenumber= 16
+console.log(favoritenumber);
+
+
+
+
+
+
+
+
 // Type your code below!
