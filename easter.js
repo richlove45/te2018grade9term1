fa7e2b14e150b39aed579bc7407b333914291d21
richlove45 <richlovenkansah@gmail.com>
2018-10-20 12:24:08 -0400

    Update easter.js
    it says things

--- easter.js
+++ easter.js
@@ -1,5 +1,17 @@
 // import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
-
-// Write your program below!
-// First line should get user input (year) and store it in variable y.
+let y = 1947
+let a = y % 19 
+let b = y / 100 
+let c = y % 100 
+let d = y / 4 
+let e = y % 4 
+let f = b + 8 / 25 
+let g = b - f + 1 / 3
+let h = 19 * a + b - d - g + 15 % 30 
+let i = c / 4 
+let k = c % 4
+let r = 32 + 2 * e + 2 * i - h - k % 7 
+let m = a + 11 * h + 22 * r / 451 
+let n = h + r - 7 * m + 114 / 31 
+let p = h + r - 7 * m + 114 % 31 
+console.log ()
